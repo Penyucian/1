0a56568cc7c2389bf83ba64b64247d5bdd7fdee1
penyucian <saintsmovin@gmail.com>
2020-03-05 22:04:15 +0700

    heu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Link} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
-      <div>
-
-      </div>
+      <BrowserRouter>
+        <nav>
+          <div className="container">
+            <div className="nav-wrapper">
+              <Link path="/" className="brand-logo">Kloea</Link>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <table className="highlight">
+            <tbody>
+              <tr>
+                <td>Taste</td>
+              </tr>
+              <tr>
+                <td>Hiatus</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="fixed-action-btn">
+            <Link class="btn-floating btn-large red waves-effect">
+              <i class="large material-icons">mode_edit</i>
+            </Link>
+          </div>
+        </div>
+      </BrowserRouter>
     )
   }
 }
